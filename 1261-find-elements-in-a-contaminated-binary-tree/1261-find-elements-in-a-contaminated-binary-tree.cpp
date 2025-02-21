db1fd3eabc712e0f801068966be41ec092365226
MKG0007 <165919021+MKG0007@users.noreply.github.com>
2025-02-22 01:24:16 +0530

    Time: 11 ms (58.77%), Space: 34.7 MB (44.78%) - LeetHub

---
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,32 @@
+// class TreeNode {
+// public:
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+    
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// };
+
+class FindElements {
+private:
+    std::unordered_set<int> recoveredValues;
+
+    void recoverTree(TreeNode* node, int value) {
+        if (node == nullptr) {
+            return;
+        }
+        node->val = value;
+        recoveredValues.insert(value);
+        recoverTree(node->left, 2 * value + 1); // Recover left child
+        recoverTree(node->right, 2 * value + 2); // Recover right child
+    }
+
+public:
+    FindElements(TreeNode* root) {
+        recoverTree(root, 0);
+    }
+
+    bool find(int target) {
+        return recoveredValues.find(target) != recoveredValues.end();
+    }
+};
