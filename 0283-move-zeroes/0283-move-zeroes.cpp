439afbecd9c190439bd39a460f452bd096e336c9
MKG0007 <165919021+MKG0007@users.noreply.github.com>
2025-03-01 09:14:57 +0530

    Time: 0 ms (100%), Space: 23.8 MB (50.06%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int size = nums.size();
+        int index = 0;
+
+        for (int i = 0 ; i<size ; i++){
+            if(nums[i] != 0){
+                swap(nums[index] , nums[i]);
+                index++;
+            }
+        }
+    }
+};
