75de9856548b8140271d222fbde0f09e70650803
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-05-21 12:18:55 +0530

    Time: 0 ms (100%), Space: 9 MB (15.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
+    bool check(char ch){
+        if(ch == '{' || ch == '(' || ch == '['){
+            return true;
+        }
+        return false;
+    }
+
     bool isValid(string s) {
-        if(s.length() == 1) return false;
-        stack<char> st;
+
         unordered_map<char , char> m;
-        m['('] = ')';
         m['{'] = '}';
         m['['] = ']';
+        m['('] = ')';
+
+        stack<char> st;
 
         for(int i = 0 ; i<s.length() ; i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+            if(check(s[i])){
                 st.push(s[i]);
             }
             else{
-                if(!st.empty()){
-                    char check = st.top();
-                    if(m[check] == s[i]){
-                        st.pop();
-                    }
-                    else{
-                        return false;
-                    }
-                    
-
-                }
-                else{
+                if(st.empty() || m[st.top()] != s[i]){
                     return false;
                 }
-                
+                st.pop();
             }
         }
-        if(!st.empty()) return false;
+
+        if(!st.empty()){
+            return false;
+        }
         return true;
     }
 };
