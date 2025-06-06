f68d265532e5aaf76bf4ab3632de34af54ac8ede
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-06-06 07:16:17 +0530

    Time: 13 ms (14.31%), Space: 12.2 MB (67.88%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char , int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+
+        char check = s[0];
+        int ans = 0;
+
+        for(int i = 0 ; i<s.length() ; i++){
+            if(m[check]<m[s[i]]){
+                ans += m[s[i]]-m[check]-m[check];
+            }else{
+                ans += m[s[i]];
+            }
+            check = s[i];
+        }
+        
+        return ans;
+    }
+};
