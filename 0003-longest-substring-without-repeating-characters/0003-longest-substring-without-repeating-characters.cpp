85c353da35fabf9b8340acd2f5aa670d926c6f5f
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-06-16 15:13:33 +0530

    Time: 8 ms (60.31%), Space: 11.8 MB (58.29%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // unordered_set<int> m;
-        string crr = "";
-        string ans = "";
-        for(int i = 0 ; i<s.length() ; i++){
-            int index = crr.find(s[i]);
-            if(index>=0){
-                crr = crr.substr(index+1 , crr.length()-1);
-                crr += s[i];
+        
+        int size = s.length();
+        if(size == 0) return 0;
+        unordered_map<int , int> m;
+        int left = 0;
+        int ans = 1;
+        for(int i = 0 ; i<size ; i++){
+            if(m.find(s[i]) != m.end()){
+                left =max(left ,  m[s[i]]+1);
             }
-            else{
-                crr += s[i];
-            }
+            m[s[i]] = i;
+            ans = max(ans , (i-left+1));
 
-            if(crr.length()>ans.length()){
-                ans = crr;
-            }
         }
-        return ans.length();
+
+        return ans;
+
+        // string crr = "";
+        // string ans = "";
+        // for(int i = 0 ; i<s.length() ; i++){
+        //     int index = crr.find(s[i]);
+        //     if(index>=0){
+        //         crr = crr.substr(index+1 , crr.length()-1);
+        //         crr += s[i];
+        //     }
+        //     else{
+        //         crr += s[i];
+        //     }
+
+        //     if(crr.length()>ans.length()){
+        //         ans = crr;
+        //     }
+        // }
+        // return ans.length();
     }
 };
