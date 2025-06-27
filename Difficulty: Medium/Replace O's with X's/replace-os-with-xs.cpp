d064cfe4aa1a6d6b0830934e99457c337afe0106
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-06-27 23:15:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Replace O's with X's/replace-os-with-xs.cpp
@@ -0,0 +1,43 @@
+// User function Template for C++
+
+class Solution {
+  public:
+  
+  void fnDfs(vector<vector<char>> &mat , vector<vector<int >> &vis , vector<pair<int , int>> &d ,  int i , int j){
+      vis[i][j] = 1;
+      
+      for(auto dd : d){
+          int di = dd.first + i;
+          int dj = dd.second + j;
+          
+          if(di>=0 && di<mat.size() && dj>=0 && dj<mat[0].size() && !vis[di][dj] && mat[di][dj] == 'O'){
+               fnDfs(mat , vis , d , di , dj);
+          }
+      }
+  }
+    vector<vector<char>> fill(vector<vector<char>>& mat) {
+        int vertex = mat.size();
+        int edge = mat[0].size();
+        vector<vector<int>> vis(vertex , vector<int>(edge , 0));
+        vector<pair<int , int>> d = { {1 , 0}  , {0 , 1} , {-1 , 0} , {0 , -1}};
+        for(int i = 0 ; i<vertex ; i++){
+            for(int j = 0 ; j<edge ; j++){
+                if(i == 0 || j == 0 || i == vertex-1 || j == edge-1){
+                    if(!vis[i][j] && mat[i][j] == 'O'){
+                        fnDfs(mat , vis , d , i , j);
+                    }
+                }
+            }
+        }
+        
+        for(int i = 0 ; i<vertex ; i++){
+            for(int j = 0  ; j<edge ; j++){
+                if(!vis[i][j]  && mat[i][j] == 'O'){
+                    mat[i][j] = 'X';
+                }
+            }
+        }
+        
+        return mat;
+    }
+};
