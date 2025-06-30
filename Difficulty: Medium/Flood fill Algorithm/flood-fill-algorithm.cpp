7adbf8d67af365521e3563ccf8dfc73c3c2fa196
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-06-30 21:54:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
+++ Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
@@ -1,35 +1,38 @@
 class Solution {
   public:
-  
-  void fnDfs(vector<vector<int>>& image , int sr, int sc, int newColor , vector<vector<int>> &visited , int oldColor){
-                                      visited[sr][sc] = 1;
-                                      image[sr][sc] = newColor;
-                                      vector<pair<int , int>> d = {{1 , 0} , {0 , 1} , {-1 , 0} , {0 , -1}};
-                                      
-                                      for(auto dd : d){
-                                          int di  = dd.first+sr;
-                                          int dj = dd.second+sc;
-                                          
-                                          if(di>=0 && di<image.size() && dj>=0 && dj<image[0].size() && !visited[di][dj] && image[di][dj] == oldColor){
-                                              fnDfs(image , di , dj , newColor , visited , oldColor);
-                                          }
-                                      }
-                                      
-                                  }
-  
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
                                   int newColor) {
-        int vertex = image.size();
-        int edge = image[0].size();
-        vector<vector<int>> visited(vertex , vector<int>(edge , 0));
+        // Code here
+        int row = image.size();
+        int col = image[0].size();
         
-        int oldColor = image[sr][sc];
         
-        fnDfs(image , sr , sc , newColor , visited , oldColor);
+        vector<vector<int>> vis(row , vector<int>(col , 0));
+        int originalColor = image[sr][sc];
+        image[sr][sc] = newColor;
+        queue<pair<int , int>> q;
+        q.push({sr , sc});
         
+        vector<pair<int , int>> dec = {{0 , 1} , {1 , 0} , {-1 , 0} , {0 , -1}};
+        vis[sr][sc] = 1;
+        
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            
+            for(auto d : dec){
+                int di = i + d.first;
+                int dj = j + d.second;
+                
+                if(di>=0 && dj>=0 && di<row && dj<col && !vis[di][dj] && image[di][dj] == originalColor){
+                    vis[di][dj] = 1;
+                    image[di][dj] = newColor;
+                    q.push({di , dj});
+                }
+            }
+        }
         
         return image;
-        
-        
     }
 };
