6c0d44b3bc3bd42a9629ed0775deb4e94d9dd46f
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-01 08:27:30 +0530

    Time: 3 ms (36.41%), Space: 8.9 MB (60.22%) - LeetHub

--- 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.cpp
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.cpp
@@ -5,17 +5,16 @@ public:
         int size = word.length();
         int check = 0;
         int ans = 1;
-        for(int i = 1 ; i<size ; i++){
-            if(word[i-1] != word[i]){
+        for (int i = 1; i < size; i++) {
+            if (word[i - 1] != word[i]) {
                 ans += check;
                 check = 0;
             }
-            if(word[i-1] == word[i]){
+            if (word[i - 1] == word[i]) {
                 check++;
             }
-
         }
-        if(check != 0){
+        if (check != 0) {
             ans += check;
         }
 
