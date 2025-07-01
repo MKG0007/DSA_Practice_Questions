d587d1c59e1605c4c0b4d2414185e9437f9554d1
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-01 12:32:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -2,44 +2,42 @@ class Solution {
   public:
     // Function to find distance of nearest 1 in the grid for each cell.
     vector<vector<int>> nearest(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
         
-        int vertex = grid.size();
-        int edges = grid[0].size();
-        vector<vector<int>> vis(vertex , vector<int>(edges , 0));
-        vector<vector<int>> dis(vertex , vector<int>(edges , 0));
-        vector<pair<int , int>> d = {{1 , 0} , {0 , 1} , {-1 , 0} , {0 , -1}};
-        queue<pair<pair<int , int> , int>> q;
         
-        for(int i = 0 ; i<vertex ; i++){
-            for(int j = 0 ; j<edges ; j++){
+        vector<vector<int>> ans(row , vector<int>(col , 0));
+        vector<vector<int>> vis(row , vector<int>(col , 0));
+        vector<pair<int , int>> dec = {{0 , 1} , {1 , 0} , {-1 , 0} , {0 , -1}};
+        queue<pair<int , int>> q;
+        
+        
+        
+        for(int i = 0 ; i<row ; i++){
+            for(int j = 0 ; j<col ; j++){
                 if(grid[i][j] == 1){
                     vis[i][j] = 1;
-                    q.push({{i , j} , 0});
-                }
-                else{
-                    vis[i][j] = 0;
+                    q.push({i , j});
                 }
             }
         }
         
         while(!q.empty()){
-            int i = q.front().first.first;
-            int j = q.front().first.second;
-            int steps = q.front().second;
+            auto n = q.front();
             q.pop();
             
-            for(auto dd : d){
-                int di = dd.first + i;
-                int dj = dd.second + j;
+            for(auto d : dec){
+                int di = n.first + d.first;
+                int dj = n.second + d.second;
                 
-                if(di>=0 && dj>=0 && di<vertex && dj<edges && !vis[di][dj]){
+                if(dj>=0 && di>=0 && dj<col && di<row && !vis[di][dj] && grid[di][dj] == 0){
                     vis[di][dj] = 1;
-                    dis[di][dj] = steps+1;
-                    q.push({{di , dj} , steps+1});
+                    q.push({di , dj});
+                    ans[di][dj] = ans[n.first][n.second]+1;
                 }
             }
         }
         
-        return dis;
+        return ans;
     }
 };
