9fe3dd0b61338bf2fd0f13a8f8e6c4a1c070ae5b
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-02 15:08:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,38 +1,47 @@
 class Solution {
   public:
   
-    bool checkByDfs( unordered_map<int , vector<int>> &adj , vector<int> &vis , vector<int> &path , int node){
-        vis[node] = 1;
-        path[node] = 1;
-        
-        for(int ele : adj[node]){
-            if(!vis[ele]){
-                if(checkByDfs(adj , vis , path , ele)) return true;
-            }
-            else if(path[ele] == 1) return true;
-        }
-        path[node] = 0;
-        return false;
-    }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         unordered_map<int , vector<int>> adj;
+        vector<int> in(V , 0);
+        queue<int> q;
         for(auto ele : edges){
             int u = ele[0];
             int v = ele[1];
             
             adj[u].push_back(v);
+            in[v]++;
         }
         
-        vector<int> vis(V , 0);
-        vector<int> path(V , 0);
-        
         for(int i = 0 ; i<V ; i++){
-            if(!vis[i]){
-                if(checkByDfs(adj , vis , path , i)) return true;
+            if(in[i] == 0){
+                q.push(i);
             }
         }
         
-        return false;
+        vector<int> ans;
+        
+        while(!q.empty()){
+            int node = q.front();
+            ans.push_back(node);
+            q.pop();
+            
+            for(int i : adj[node]){
+                in[i]--;
+                if(in[i] == 0){
+                    q.push(i);
+                }
+            }
+        }
+        
+        if(ans.size() == V) return false;
+        
+        return true;
+        
+        
+    
+        
+        
         
     }
 };
