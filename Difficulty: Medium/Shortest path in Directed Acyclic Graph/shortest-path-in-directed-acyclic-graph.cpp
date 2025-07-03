490b3e14942c2c6283240a596f9f3299ae0c8095
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-03 12:32:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -2,60 +2,55 @@
 class Solution {
   public:
   
-    void dfs(unordered_map<int , vector<pair<int , int>>> &adj , stack<int> &st , vector<int> &vis , int node){
-        vis[node] = 1;
-        
-        for(auto ele : adj[node]){
-            int n = ele.first;
-            if(!vis[n]){
-                dfs(adj , st , vis , n);
-            }
-        }
-        
-        st.push(node);
-        
-    }
+  void dfs(unordered_map<int , vector<pair<int , int>>> &adj , vector<int> &vis , stack<int> &st , int node){
+      vis[node] = 1;
+      
+      for(auto ele : adj[node]){
+          if(!vis[ele.first]){
+              dfs(adj , vis , st , ele.first);
+          }
+      }
+      
+      st.push(node);
+  }
     vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
-        unordered_map<int , vector<pair<int , int>>> adj;
-        vector<int> vis(V , 0);
-        stack<int> st;
-        
-        for(int i = 0 ; i<E ; i++){
-                int u = edges[i][0];
-                int v = edges[i][1];
-                int w = edges[i][2];
-                
-                adj[u].push_back({v , w});
-        }
-        
-        vector<int> dis(V , INT_MAX);
-        dis[0] = 0;
-        for(int i = 0 ; i<V ; i++){
-                if(!vis[i]){
-                    dfs(adj , st ,vis ,  i);
-                }
-        }
-        
-        
-        
-        while(!st.empty()){
-            int i = st.top();
-            st.pop();
+            unordered_map<int , vector<pair<int , int>>> adj;
             
-            for(auto ele : adj[i]){
-                int n = ele.first;
-                int w = ele.second;
-                if(dis[i] != INT_MAX && (dis[i] + w) < dis[n]){
-                    dis[n] = dis[i] + w;
+            for(auto ele : edges){
+                int u = ele[0];
+                int v = ele[1];
+                int w = ele[2];
+                adj[u].push_back({v , w});
+            }
+            vector<int> vis(V , 0);
+            stack<int> st;
+            
+            for(int i =0 ; i<V ; i++){
+                if(!vis[i]){
+                    dfs(adj , vis , st , i);
                 }
             }
-        }
-        
-        for(int i = 0 ; i<V ; i++){
-            if(dis[i] == INT_MAX){
-                dis[i] = -1;
+            
+            vector<int> dis(V , 1e9);
+            dis[0] = 0;
+            
+            while(!st.empty()){
+                int n = st.top();
+                st.pop();
+                for(auto ele : adj[n]){
+                    if((dis[n] + ele.second) < dis[ele.first]){
+                        dis[ele.first] = dis[n] + ele.second;
+                    }
+                }
             }
-        }        
-        return dis;
+            
+            for(int i = 0 ; i<V ; i++){
+                if(dis[i] == 1e9){
+                    dis[i] = -1;
+                }
+            }
+            
+            return dis;
+        
     }
 };
