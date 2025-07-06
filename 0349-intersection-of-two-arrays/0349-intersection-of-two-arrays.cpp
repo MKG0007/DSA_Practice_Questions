4e9ce86b07a1a74507e2c9fc92f9dd50afcdd755
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-06 19:33:51 +0530

    Time: 1 ms (50.74%), Space: 14.5 MB (37.7%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int size1 = nums1.size();
+        int size2 = nums2.size();
+        unordered_map<int , int> m;
+        vector<int> ans;
+
+        for(int i = 0 ; i<size2 ; i++){
+            m[nums2[i]]++;
+        }
+
+        for(int i = 0 ; i<size1 ; i++){
+            if(m.find(nums1[i]) != m.end()){
+                if(m[nums1[i]] != 0){
+                    ans.push_back(nums1[i]);
+                }
+                m[nums1[i]] = 0;
+            }
+        }
+        return ans;
+    }
+};
