6bd94095b93fb4ec84f4c43d28472f48c3cff627
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-10 14:51:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Level order traversal/level-order-traversal.cpp
+++ Difficulty: Easy/Level order traversal/level-order-traversal.cpp
@@ -17,24 +17,28 @@ class Node {
 class Solution {
   public:
     vector<vector<int>> levelOrder(Node *root) {
-        
-        
-        queue<Node*> q;
         vector<vector<int>> ans;
+        if(root == NULL) return ans;
+        queue<Node*> q;
         q.push(root);
         
         while(!q.empty()){
             int size = q.size();
-            vector<int> a;
+            
+            vector<int> temp;
+            
             for(int i = 0 ; i<size ; i++){
-                Node* n = q.front();
-                if(n->left != NULL) q.push(n->left);
-                if(n->right != NULL) q.push(n->right);
-                a.push_back(n->data);
+                Node* node = q.front();
                 q.pop();
+                temp.push_back(node->data);
+                
+                if( node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
             }
-            ans.push_back(a);
+            ans.push_back(temp);
         }
+        
         return ans;
+        
     }
 };
