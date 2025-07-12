955b3485eebc83f6e5c9cd32502b919589961129
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-12 15:46:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -1,19 +1,20 @@
-// User function template for C++
 class Solution {
   public:
     bool twoSum(vector<int>& arr, int target) {
         int size = arr.size();
-        unordered_set<int> sm;
         
-        for(int i = 0 ; i<size ; i++){ 
-            int num = target-arr[i];
-            if(sm.find(num) != sm.end()){
+        unordered_set<int> s;
+        
+        for(int i = 0 ; i<size ; i++){
+            int num1 = arr[i];
+            int num2 = target-arr[i];
+            
+            if(s.find(num2) != s.end()){
                 return true;
             }
-            
-            sm.insert(arr[i]);
-            
+            s.insert(arr[i]);
         }
+        
         return false;
     }
 };
