7d13f5a3b00b930ffcd97338120bbbd6f6bb7ea5
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-22 16:30:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -1,85 +1,38 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
   
-  void merge(vector<int> &arr , int low , int mid , int high){
-      int i = low;
-      int j = mid+1;
-      vector<int> temp;
-      while(i<=mid && j<=high){
-          if(arr[i]<=arr[j]){
-              temp.push_back(arr[i]);
-              i++;
-          }
-          else{
-              temp.push_back(arr[j]);
-              j++;
-          }
-      }
-      
-      while(i<=mid){
-          temp.push_back(arr[i]);
-          i++;
-      }
-      
-      while(j<=high){
-          temp.push_back(arr[j]);
-          j++;
-      }
-      
-      for(int i = low ; i<=high ; i++){
-          arr[i] = temp[i-low];
-      }
-      
-  }
-  
-  
-  void msort(vector<int> &arr , int low , int high){
-      if(low>=high){
-          return;
-      }
-      int mid = (low+high)/2;
-      msort(arr , low , mid);
-      msort(arr , mid+1 , high);
-      
-      merge(arr , low , mid , high);
-  }
+    void merge(vector<int> &arr , int left , int mid , int right){
+        vector<int> ans;
+        int l = left;
+        int m = mid;
+        mid++;
+        while(left<=m && mid<=right){
+            if(arr[left]<=arr[mid]){
+                ans.push_back(arr[left]);
+                left++;
+            }
+            else{
+                ans.push_back(arr[mid]);
+                mid++;
+            }
+        }
+        
+        while(left<=m) ans.push_back(arr[left]), left++;
+        while(mid<=right) ans.push_back(arr[mid]), mid++;
+        
+        for(int i = l ; i<=right ; i++){
+            arr[i] = ans[i-l];
+        }
+        
+    }
     void mergeSort(vector<int>& arr, int l, int r) {
-        msort(arr , l , r);
+        if(l >=r){
+            return ;
+        }
+        int mid = (l+r)/2;
+        
+        mergeSort(arr , l , mid);
+        mergeSort(arr , mid+1 , r);
+        merge(arr , l , mid , r);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        obj.mergeSort(arr, 0, arr.size() - 1);
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
