0346768d7cc818c931876f77513866e1722d6084
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-30 00:52:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.cpp
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -4,22 +4,19 @@ class Solution {
     // Function to find the leaders in the array.
   public:
     vector<int> leaders(vector<int>& arr) {
-        int size = arr.size();
         // Code here
+        int size = arr.size();
         vector<int> ans;
-        if(size == 0) return ans;
-        if(size == 1) return {arr[0]};
-        
-        int maxi = INT_MIN;
+        int crr = INT_MIN;
         for(int i = size-1 ; i>=0 ; i--){
-            if(maxi<=arr[i]){
+            if(arr[i]>=crr){
                 ans.push_back(arr[i]);
             }
-            maxi = max(maxi , arr[i]);
+            
+            crr = max(crr , arr[i]);
         }
         
-        reverse(ans.begin(), ans.end());
-        
+        reverse(ans.begin() , ans.end());
         return ans;
     }
 };
