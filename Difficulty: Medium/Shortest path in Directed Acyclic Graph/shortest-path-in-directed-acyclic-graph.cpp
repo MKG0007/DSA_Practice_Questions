fe9975e243c42a6e0dfd212ab3e8e64a1f6c5ccd
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-08-13 15:27:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -1,8 +1,7 @@
 // User function Template for C++
 class Solution {
   public:
-  
-  void dfs(unordered_map<int , vector<pair<int , int>>> &adj , vector<int> &vis , stack<int> &st , int node){
+  void dfs(unordered_map<int , vector<pair<int , int>>> &adj , vector<int> & vis , stack<int> &st , int node){
       vis[node] = 1;
       
       for(auto ele : adj[node]){
@@ -14,43 +13,50 @@ class Solution {
       st.push(node);
   }
     vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
-            unordered_map<int , vector<pair<int , int>>> adj;
-            
-            for(auto ele : edges){
-                int u = ele[0];
-                int v = ele[1];
-                int w = ele[2];
-                adj[u].push_back({v , w});
+        unordered_map<int , vector<pair<int , int>>> adj;
+        
+        for(auto ele : edges){
+            adj[ele[0]].push_back({ele[1] , ele[2]});
+        }
+        
+        vector<int> vis(V , 0);
+        stack<int> st;
+        
+        for(int i = 0 ; i<V ; i++){
+            if(!vis[i]){
+                dfs(adj , vis , st , i);
             }
-            vector<int> vis(V , 0);
-            stack<int> st;
+        }
+        
+        vector<int> dis(V , 1e9);
+        
+        dis[0] = 0;
+        
+        while(!st.empty()){
+            int node = st.top();
+            st.pop();
             
-            for(int i =0 ; i<V ; i++){
-                if(!vis[i]){
-                    dfs(adj , vis , st , i);
+            for(auto ele : adj[node]){
+                int i = ele.first;
+                int wt = ele.second;
+                
+                if(dis[i]>dis[node]+wt){
+                    dis[i] = dis[node]+wt;
                 }
             }
-            
-            vector<int> dis(V , 1e9);
-            dis[0] = 0;
-            
-            while(!st.empty()){
-                int n = st.top();
-                st.pop();
-                for(auto ele : adj[n]){
-                    if((dis[n] + ele.second) < dis[ele.first]){
-                        dis[ele.first] = dis[n] + ele.second;
-                    }
-                }
+        }
+        
+        
+        
+        for(int i = 0 ; i<V ; i++){
+            if(dis[i] == 1e9){
+                dis[i] = -1;
             }
-            
-            for(int i = 0 ; i<V ; i++){
-                if(dis[i] == 1e9){
-                    dis[i] = -1;
-                }
-            }
-            
-            return dis;
+        }
+        
+        return dis;
+        
+        
         
     }
 };
