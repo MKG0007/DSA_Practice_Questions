78cb6c11abe365e0d96161c11410d60df4521cd7
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-08-26 18:36:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -1,20 +1,18 @@
-// User function Template for C++
 class Solution {
   public:
-  int fibo(int num , vector<int> &dp){
-      if(num <=1) return num;
-      
-      if(dp[num] != -1){
-          return dp[num];
-      }
-      dp[num] = fibo(num-1 , dp)+fibo(num-2 , dp);
-      return dp[num];
-      
-  }
     int nthFibonacci(int n) {
-        vector<int> dp(n+1 , -1);
+        if(n == 0 || n == 1) return n;
         
-        return fibo(n , dp);
+        int prev = 1;   // stores F(i-1)
+        int prev2 = 0;  // stores F(i-2)
+        int crr = 0;    // stores current F(i)
         
+        for(int i = 2; i <= n; i++){
+            crr = prev + prev2;  // Fibonacci relation: F(i) = F(i-1) + F(i-2)
+            prev2 = prev;        // shift: update F(i-2) → old F(i-1)
+            prev = crr;          // shift: update F(i-1) → current value
+        }
+        
+        return prev;
     }
 };
