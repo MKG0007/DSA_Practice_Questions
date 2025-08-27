a2acc1fee498567f5a40c701acc36c293ab47b17
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-08-27 14:45:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,23 +1,25 @@
 class Solution {
   public:
   
-  int minPath(vector<int> &height , vector<int> &dp , int num){
-      if(num == 0){
-          return 0;
-      }
-      if(dp[num] != -1) return dp[num];
-      int left = minPath(height , dp , num-1) +abs(height[num] - height[num-1]);
-      int right = INT_MAX;
-      if(num > 1) {right = minPath(height , dp , num-2) +abs(height[num]- height[num-2]);}
-      dp[num] = min(left , right);
-      return dp[num];
-  }
   
+    int cost(vector<int> &dp , vector<int> &arr , int index){
+        if(index == 0) return 0;
+        if(dp[index] != -1) return dp[index];
+        
+        int left = cost(dp , arr ,index-1) + abs(arr[index] - arr[index-1]);
+        int right = INT_MAX;
+        if(index>1) right = cost(dp , arr , index-2) + abs(arr[index]-arr[index-2]);
+        
+        return dp[index] = min(left , right);
+    }
     int minCost(vector<int>& height) {
         int n = height.size();
+        
         vector<int> dp(n+1 , -1);
         
-        return minPath(height , dp , n-1);
+        int result = cost(dp , height , n-1);
+        
+        return result;
         
     }
 };
