676069386286314a8a5328a4abaf212f2dcf785b
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-08-28 09:57:25 +0530

    Time: 3 ms (31.75%), Space: 15.8 MB (9.28%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
 
-    void totalSubset(vector<int> &nums , vector<int> subset , vector<vector<int>> &ans , int index , int size){
-        if(index == size){
-            ans.push_back(subset);
+    void find(vector<vector<int>> &ans , vector<int> sub , vector<int> &nums ,  int index){
+        if(index == nums.size()){
+            ans.push_back(sub);
             return ;
         }
-        subset.push_back(nums[index]);
-        totalSubset(nums , subset , ans , index+1 , size);
-        subset.pop_back();
-        totalSubset(nums , subset , ans , index+1 , size);
+
+        sub.push_back(nums[index]);
+        find(ans , sub , nums , index+1);
+        sub.pop_back();
+        find(ans , sub , nums , index+1);
 
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans ;
-        vector<int> subset;
-        int size = nums.size();
-
-        totalSubset(nums , subset ,ans , 0 , size);
-
+        vector<vector<int>> ans;
+        vector<int> sub;
+        find(ans , sub , nums , 0);
         return ans;
-        
     }
 };
