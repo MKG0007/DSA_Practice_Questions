ed0ab00307ccb469a469a8f5c34ece64bbe64ef1
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-08-29 16:47:18 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,8 +12,19 @@ public:
     }
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size() , -1);
+        dp[0] = nums[0];
 
-        return maxMoney(dp , nums , nums.size()-1);
+        for(int i = 1 ; i<nums.size() ; i++){
+            int pick = nums[i];
+            if(i-2>=0){
+                pick += dp[i-2];
+            }
+            int notpick = 0 + dp[i-1];
+            dp[i] = max(pick , notpick);
+
+        }
+
+        return dp[nums.size()-1];
         
     }
 };
