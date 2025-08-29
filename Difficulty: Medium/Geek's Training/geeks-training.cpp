24681d3bd4d06418855b3487dce350b16aeb57fa
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-08-29 19:18:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+  
+    int maxPoints(vector<vector<int>> &dp , vector<vector<int>> &arr , int index , int prev){
+        if(index == 0){
+            int maxi = INT_MIN;
+            for(int i = 0 ; i<3 ; i++){
+                if(i == prev){
+                    continue;
+                }
+                maxi = max(maxi , arr[0][i]);
+            }
+            
+            return maxi;
+        }
+        if(dp[index][prev] != -1) return dp[index][prev];
+        
+        int maxi = INT_MIN;
+        
+        for(int i = 0 ; i<3 ; i++){
+            if(i == prev){
+                continue;
+            }
+            int task = arr[index][i] + maxPoints(dp , arr , index-1 , i);
+            
+            maxi = max(maxi , task);
+        }
+        return dp[index][prev] =  maxi;
+    }
+    int maximumPoints(vector<vector<int>>& arr) {
+        int row = arr.size();
+        int col = arr[0].size();
+        
+        vector<vector<int>> dp(row , vector<int>(col+1 , -1));
+        
+        return maxPoints(dp ,arr ,  row-1 , 3);
+        
+    }
+};
