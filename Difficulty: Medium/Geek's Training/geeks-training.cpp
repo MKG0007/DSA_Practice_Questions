28bd8339aa2489c0395f90755d082b1d819c5522
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-08-30 16:18:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -1,39 +1,37 @@
 class Solution {
   public:
   
-    int maxPoints(vector<vector<int>> &dp , vector<vector<int>> &arr , int index , int prev){
-        if(index == 0){
-            int maxi = INT_MIN;
-            for(int i = 0 ; i<3 ; i++){
-                if(i == prev){
-                    continue;
-                }
-                maxi = max(maxi , arr[0][i]);
-            }
-            
-            return maxi;
-        }
-        if(dp[index][prev] != -1) return dp[index][prev];
-        
-        int maxi = INT_MIN;
-        
-        for(int i = 0 ; i<3 ; i++){
-            if(i == prev){
-                continue;
-            }
-            int task = arr[index][i] + maxPoints(dp , arr , index-1 , i);
-            
-            maxi = max(maxi , task);
-        }
-        return dp[index][prev] =  maxi;
-    }
+  int findMax(vector<vector<int>> &dp , vector<vector<int>> &arr , int index , int prev){
+      if(index == 0){
+          int maxChoice = INT_MIN;
+          for(int i = 0 ; i<3 ; i++){
+              if(i == prev) continue;
+              
+              maxChoice = max(maxChoice , arr[0][i]);
+          }
+          return maxChoice;
+      }
+      
+      if(dp[index][prev] != -1) return dp[index][prev];
+      int maxChoice = INT_MIN;
+      
+      for(int i = 0 ; i<3 ; i++){
+          if(prev == i) continue;
+          
+          int path = arr[index][i] + findMax(dp , arr , index-1 , i);
+          maxChoice = max(maxChoice , path);
+          
+      }
+      
+      return dp[index][prev] = maxChoice;
+  }
     int maximumPoints(vector<vector<int>>& arr) {
         int row = arr.size();
         int col = arr[0].size();
         
-        vector<vector<int>> dp(row , vector<int>(col+1 , -1));
+        vector<vector<int>> dp(row+1 , vector<int>(col+1 , -1));
         
-        return maxPoints(dp ,arr ,  row-1 , 3);
+        return findMax(dp , arr , row-1 , 3);
         
     }
 };
