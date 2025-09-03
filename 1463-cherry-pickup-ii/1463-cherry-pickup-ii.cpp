ad1614de8596fc87b2b1e8294332b60f001912c9
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-03 15:07:08 +0530

    Time: 50 ms (57.26%), Space: 20.9 MB (53.03%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    int maxPickup(int i , int j1 , int j2 , int row  , int col , vector<vector<int>>& grid , vector<vector<vector<int>>> &dp){
-        if(i>=row || j1<0 || j1>=col || j2<0 || j2>=col){
-            return -1e8;
-        }
 
-        if(i == row-1) {
-            if(j1 == j2) return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
 
+    int maxCount(vector<vector<int>> &grid , vector<vector<vector<int>>> & dp , int i , int j1 , int j2 , int row , int col){
+        if(j1<0 || j2<0 || j2>=col || j1>=col) return -1e7;
+
+        if(i == row-1){
+            int val = grid[i][j1];
+            if(j1 == j2) return val;
+
+            return val+grid[i][j2];
         }
-        
         if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        int maxi = 0;
 
-        // explore all the path
-        int maxi = INT_MIN;
         for(int dj1 = -1 ; dj1<=1 ; dj1++){
             for(int dj2 = -1 ; dj2<=1 ; dj2++){
-                int value = 0;
-                if(j1 == j2) value = grid[i][j1];
-                else value = grid[i][j1] + grid[i][j2];
+                int val = 0;
+                if(j1 == j2) val = grid[i][j1];
+                else val = grid[i][j1]+grid[i][j2];
 
-                value += maxPickup(i+1 , j1+dj1 , j2+dj2 , row , col , grid , dp);
-                maxi = max(maxi , value);
-
+                val += maxCount(grid , dp , i+1 , j1+dj1 , j2+dj2 , row , col);
+            maxi = max(maxi , val);
             }
         }
 
         return dp[i][j1][j2] = maxi;
-
-
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
 
         vector<vector<vector<int>>> dp(row , vector<vector<int>>(col , vector<int>(col , -1)));
-
-        return maxPickup(0 , 0 , col-1 , row , col , grid , dp);
+        return maxCount(grid , dp , 0 , 0 , col-1 , row , col);
         
     }
 };
