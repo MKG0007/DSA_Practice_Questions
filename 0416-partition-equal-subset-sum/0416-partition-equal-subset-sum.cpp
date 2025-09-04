1ae60a5e0935bd0ca20c8f8a5da7136a2bf3bd7e
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-04 15:02:49 +0530

    Time: 127 ms (62.19%), Space: 110.6 MB (33.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
+    bool check(vector<int> &nums , vector<vector<int>> &dp , int target , int index){
+        if(target == 0) return true;
+        if(index == 0) return target == nums[0];
+        if(dp[index][target] != -1) return dp[index][target];
+
+        int pick = false;
+        if(nums[index]<target) pick = check(nums , dp , target-nums[index] , index-1);
+
+        int notpick = check(nums , dp , target , index-1);
+
+        return dp[index][target] = pick || notpick;
+    }
     bool canPartition(vector<int>& nums) {
+        int size = nums.size();
+        int totalSum = 0;
+        for(int i = 0 ; i<size ; i++) totalSum += nums[i];
 
-        int sum = 0, n = nums.size();
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-        }
-        if (sum % 2 != 0)
-            return false;
+        if(totalSum%2 != 0) return false;
 
-        bitset<10001> bit(1);
-        for (auto i : nums) {
-            bit |= bit << i;
-            if (bit[sum / 2])
-                return true;
-        }
-        return bit[sum / 2];
+        int t = totalSum/2;
+        vector<vector<int>> dp(size , vector<int>(t+1 , -1));
+
+        if(check(nums , dp , t , size-1)) return true;
+
+        return false;
+        
     }
 };
