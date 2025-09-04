820d01d00231193b8e12e555ddd6be46d5a8b807
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-04 15:03:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+     bool check(vector<int> &nums , vector<vector<int>> &dp , int target , int index){
+        if(target == 0) return true;
+        if(index == 0) return target == nums[0];
+        if(dp[index][target] != -1) return dp[index][target];
+
+        int pick = false;
+        if(nums[index]<target) pick = check(nums , dp , target-nums[index] , index-1);
+
+        int notpick = check(nums , dp , target , index-1);
+
+        return dp[index][target] = pick || notpick;
+    }
+    bool equalPartition(vector<int>& nums) {
+        int size = nums.size();
+        int totalSum = 0;
+        for(int i = 0 ; i<size ; i++) totalSum += nums[i];
+
+        if(totalSum%2 != 0) return false;
+
+        int t = totalSum/2;
+        vector<vector<int>> dp(size , vector<int>(t+1 , -1));
+
+        if(check(nums , dp , t , size-1)) return true;
+
+        return false; 
+    }
+};
