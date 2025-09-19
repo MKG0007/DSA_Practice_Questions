4d5df002d7bde72903c4b85a54788c51033d3c43
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-19 08:46:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        stack<pair<int , int>> st;
-        int row = matrix.size();
-        int col = matrix[0].size();
-
+    void setZeroes(vector<vector<int>>& arr) {
+        int row = arr.size();
+        int col = arr[0].size();
+        int ccol = 1;
         for(int i = 0 ; i<row ; i++){
             for(int j = 0 ; j<col ; j++){
-                if(matrix[i][j] == 0){
-                    st.push({i , j});
+                if(arr[i][j] == 0){
+                    arr[i][0] = 0;
+
+                    if(j>0) arr[0][j] = 0;
+                    else ccol = 0;
                 }
             }
         }
 
-        while(!st.empty()){
-            int i = st.top().first;
-            int j = st.top().second;
-            int tempi = 0;
-            int tempj = 0;
-            st.pop();
+        for(int i = 1 ; i<row ; i++){
+            for(int j = 1 ; j<col ; j++){
+                if(arr[0][j] == 0 || arr[i][0] == 0) arr[i][j] = 0;
 
-            while(tempj != col){
-                matrix[i][tempj] = 0;
-                tempj++;
-            }
-
-            while(tempi != row){
-                matrix[tempi][j] = 0;
-                tempi++;
             }
         }
+
+        if(arr[0][0] == 0){
+            for(int i = 0 ; i<row ; i++){
+                arr[i][0] = 0;
+            }
+        }
+
+        if(ccol == 0){
+            for(int i = 0 ; i<col ; i++){
+                arr[0][i] = 0;
+            }
+        }
+        
         
     }
 };
