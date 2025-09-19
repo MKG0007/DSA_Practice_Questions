00370ca70f1ac69053a0ea930549d581de529f5f
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-19 23:39:15 +0530

    Time: 0 ms (100%), Space: 12.4 MB (8.9%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int index1 = 0;
-        int index2 = 0;
-        vector<int> temp;
+        int i = m - 1;      // last index of nums1’s valid elements
+        int j = n - 1;      // last index of nums2
+        int k = m + n - 1;  // last index of nums1 (total size)
 
-        while(index1<m && index2<n){
-            if(nums1[index1]<=nums2[index2]){
-                temp.push_back(nums1[index1]);
-                index1++;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
             }
-            else{
-                temp.push_back(nums2[index2]);
-                index2++;
-            }
+            k--;
         }
 
-        while(index2<n) temp.push_back(nums2[index2++]);
-        while(index1<m) temp.push_back(nums1[index1++]);
-
-        for(int i = 0 ; i<temp.size() ; i++){
-            nums1[i] = temp[i];
+        // If nums2 still has elements left
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
-
-        
-        
     }
 };
