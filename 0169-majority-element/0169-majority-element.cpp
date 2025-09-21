c2d3ce4f2dfb9f30a73d5e13edc56e9d775bccc5
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-21 22:00:36 +0530

    Time: 0 ms (100%), Space: 28.1 MB (62.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,19 +4,15 @@ public:
         int size = nums.size();
 
         int freq = 0;
-        int ans = 0;
+        int number = 0;
 
         for(int i = 0 ; i<size ; i++){
-            if(freq == 0){
-                ans = nums[i];
-            }
-            if(ans == nums[i]){
-                freq++;
-            }
-            else{
-                freq--;
-            }
+            if(freq == 0) number = nums[i], freq = 1;
+            else if(nums[i] == number) freq++;
+            else freq--;
         }
-     return ans;   
+
+        return number;
+        
     }
 };
