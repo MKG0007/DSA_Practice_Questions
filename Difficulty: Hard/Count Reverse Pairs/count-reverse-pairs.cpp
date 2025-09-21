56c86ac9cd293de5280b24f77015620086733c55
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-21 23:36:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Count Reverse Pairs/count-reverse-pairs.cpp
+++ Difficulty: Hard/Count Reverse Pairs/count-reverse-pairs.cpp
@@ -1,69 +1,61 @@
 class Solution {
-public:
-    int countRevPairs(vector<int>& arr) {
-        vector<int> temp = arr;
-        return mergeSort(temp, 0, temp.size() - 1);
-    }
-    
-private:
-    int mergeSort(vector<int>& arr, int left, int right) {
-        if (left >= right) return 0;
-        
-        int mid = left + (right - left) / 2;
-        int count = 0;
-        
-        count += mergeSort(arr, left, mid);
-        count += mergeSort(arr, mid + 1, right);
-        
-        count += countCrossPairs(arr, left, mid, right);
-        
-        merge(arr, left, mid, right);
-        
-        return count;
-    }
-    
-    int countCrossPairs(vector<int>& arr, int left, int mid, int right) {
-        int count = 0;
-        int j = mid + 1;
-        
-        for (int i = left; i <= mid; i++) {
-            while (j <= right && (long long)arr[i] > 2LL * arr[j]) {
+  public:
+
+    void merge(vector<int> &nums , int low , int mid , int high){
+        int i = low ;
+        int j = mid+1;
+        vector<int> temp;
+
+        while(i<=mid && j<=high){
+            if(nums[i]<= nums[j]){
+                temp.push_back(nums[i]);
+                i++;
+            }
+            else{
+                temp.push_back(nums[j]);
                 j++;
             }
-            count += (j - (mid + 1));
         }
-        
-        return count;
-        
+
+        while(j<=high) temp.push_back(nums[j]) , j++;
+
+        while(i<=mid) temp.push_back(nums[i]) , i++;
+
+        for(int i = low ; i<=high ; i++){
+            nums[i] = temp[i-low];
+        }
+
+
     }
-    
-    void merge(vector<int>& arr, int left, int mid, int right) {
-        vector<int> leftArr(mid - left + 1);
-        
-        vector<int> rightArr(right - mid);
-        
-        for (int i = 0; i < leftArr.size(); i++) {
-            leftArr[i] = arr[left + i];
+
+    int countPairs(vector<int> &nums , int low , int mid , int high){
+        int count = 0;
+        int right = mid+1;
+        for(int i = low ; i<=mid ; i++){
+            while(right<=high && nums[i]> (long long)2*nums[right]) right++;
+            count += right-(mid+1);
         }
-        for (int i = 0; i < rightArr.size(); i++) {
-            rightArr[i] = arr[mid + 1 + i];
-        }
+
+        return count;
+    }
+
+
+    int mergeSortCount(vector<int> &nums , int low , int high){
+        int count = 0;
+        if(low>=high) return count;
+
+        int mid = (low+high)/2;
+
+        count += mergeSortCount(nums , low , mid);
+        count += mergeSortCount(nums , mid+1 , high);
+        count += countPairs(nums , low , mid , high);
+        merge(nums , low , mid , high);
+        return count;
+    }
+    int countRevPairs(vector<int> &arr) {
+        int size = arr.size();
         
-        int i = 0, j = 0, k = left;
+        return mergeSortCount(arr , 0 , size-1);
         
-        while (i < leftArr.size() && j < rightArr.size()) {
-            if (leftArr[i] <= rightArr[j]) {
-                arr[k++] = leftArr[i++];
-            } else {
-                arr[k++] = rightArr[j++];
-            }
-        }
-        
-        while (i < leftArr.size()) {
-            arr[k++] = leftArr[i++];
-        }
-        while (j < rightArr.size()) {
-            arr[k++] = rightArr[j++];
-        }
     }
 };
