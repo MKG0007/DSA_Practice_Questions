143ee933567d210683299bd0ff42c7a0c1fab3f0
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-21 22:44:38 +0530

    Time: 0 ms (100%), Space: 23.4 MB (95.2%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& arr) {
-       int size = arr.size();
-        unordered_map<int , int> m;
-        vector<int> ans;
-        
+    vector<int> majorityElement(vector<int>& nums) {
+        int size = nums.size();
+
+        int count1 = 0;
+        int count2 = 0;
+
+        int num1 = 0;
+        int num2 = 0;
+
         for(int i = 0 ; i<size ; i++){
-            m[arr[i]]++;
+            if(count1 == 0 && num2 != nums[i]) num1 = nums[i] , count1 = 1;
+            else if (count2 == 0 && num1 != nums[i]) num2 = nums[i] , count2 = 1;
+            else if(num1 == nums[i]) count1++;
+            else if(num2 == nums[i]) count2++;
+            else count1--, count2--;
         }
-        
-        for(auto ele : m){
-            if(ele.second>size/3){
-                ans.push_back(ele.first);
+        count1 = 0;
+        count2 = 0;
+        for(int i = 0 ; i<size ; i++){
+            if(num1 == nums[i]){
+                count1++;
+            }
+            else if(num2 == nums[i]){
+                count2++;
             }
         }
-        // sort(ans.begin() , ans.end());
-        
-        return ans;  
+        vector<int> ans;
+        if(count1> size / 3) ans.push_back(num1);
+        if(count2> size / 3) ans.push_back(num2);
+
+        return ans;
     }
 };
