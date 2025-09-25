d0b9d36b8d06fe78e7e0c42076ac677babfee200
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-25 07:59:10 +0530

    Time: 0 ms (100%), Space: 13.5 MB (22.22%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -8,7 +8,9 @@ public:
         for(int j = 0 ; j<row ; j++) dp[row-1][j] = arr[row-1][j];
         
         for(int i = row-2 ; i>=0 ; i--){
+
             for(int j = i ; j>=0 ; j--){
+                
                 int p1 = arr[i][j] + dp[i+1][j];
                 int p2 = arr[i][j] + dp[i+1][j+1];
                 
