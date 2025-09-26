0600ec50102996819cbd0c495e08ebcce413b478
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-09-26 17:15:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Three Sum/three-sum.cpp
+++ Difficulty: Medium/Three Sum/three-sum.cpp
@@ -2,32 +2,33 @@
 class Solution {
   public:
     vector<vector<int>> triplets(vector<int> &arr) {
-        vector<vector<int>> ans;
         int size = arr.size();
-        if(size == 0) return ans;
         sort(arr.begin() , arr.end());
+        
+        vector<vector<int>> ans;
         
         for(int i = 0 ; i<size ; i++){
             if(i>0 && arr[i] == arr[i-1]) continue;
+            
             int j = i+1;
             int k = size-1;
+            
             while(j<k){
                 int sum = arr[i]+arr[j]+arr[k];
+                
                 if(sum == 0){
                     ans.push_back({arr[i] , arr[j] , arr[k]});
                     j++;
                     k--;
                     while(j<k && arr[j] == arr[j-1]) j++;
                     while(j<k && arr[k] == arr[k+1]) k--;
+                    
                 }
-                else if(sum<0){
-                    j++;
-                }
-                else{
-                    k--;
-                }
+                else if(sum>0) k--;
+                else j++;
             }
         }
+        
         
         return ans;
         
