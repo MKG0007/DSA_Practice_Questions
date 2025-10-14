d6497d0b15578433f201093fe5b74919fc7bab6a
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-10-14 23:08:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.cpp
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -1,27 +1,25 @@
 class Solution {
   public:
-  
-  int maxsum(vector<int> &dp , vector<int> &arr , int index){
-      if(index < 0) return 0;
-      if(index == 0) return arr[0];
-      
-      if(dp[index] != -1) return dp[index];
-      int pick = arr[index] + maxsum(dp , arr , index-2);
-      int notpick = 0 + maxsum(dp , arr , index-1);
-      
-      return dp[index] = max(pick , notpick);
-      
-  }
     int findMaxSum(vector<int>& arr) {
-        
-        
+        // code here
         int size = arr.size();
         
-        vector<int> dp(size , -1);
+        int prev = arr[0];
+        int prev2 = 0;
         
-        return maxsum(dp , arr , size-1);
+        for(int i = 1 ; i<size ; i++){
+            int pick = arr[i];
+            
+            if(i>1) pick += prev2;
+            int npick = 0 + prev;
+            
+            int crr = max(pick , npick);
+            
+            prev2 = prev;
+            prev = crr;
+            
+        }
         
-        
-        
+        return prev;
     }
 };
