73927df77e173040a92a736999692422d485ff76
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-10-15 10:45:20 +0530

    Time: 0 ms (100%), Space: 16.3 MB (40.28%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
+int maxsum(vector<vector<int>>& grid , vector<vector<int>>& dp , int row , int col){
+    if(row == 0 && col == 0) return grid[row][col];
+    if(row<0 || col <0) return 1e6;
+    if(dp[row][col] != -1) return dp[row][col];
+    int left = grid[row][col] + maxsum(grid , dp , row , col-1);
+    int right = grid[row][col] + maxsum(grid , dp , row-1 , col);
 
-    int path(vector<vector<int>> &dp , vector<vector<int>> &arr , int i , int j , int row , int col){
-        if(i<0 || i>row || j<0 || j>col) return 1e7;
+    return dp[row][col] = min(left , right);
 
-        if(i == 0 && j == 0) return arr[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int p1 = arr[i][j] + path(dp , arr , i-1 , j , row , col);
-        int p2 = arr[i][j] + path(dp , arr , i , j-1 , row , col);
 
-        return dp[i][j] = min(p1 , p2);
-    }
+
+
+}
     int minPathSum(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
-        vector<vector<int>> dp(row+1 , vector<int>(col+1 , -1));
 
-        return path(dp , grid , row-1 , col-1 , row , col);
+        vector<vector<int>> dp(row , vector<int>(col , -1));
 
-        
+        return maxsum(grid , dp , row-1 , col-1);
     }
 };
