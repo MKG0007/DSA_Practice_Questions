15d6b83056b99d5ac6f995c1fd46ddf8612d6044
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-10-15 22:58:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+  
+    int totalWays(vector<int> &arr , int t , vector<vector<int>> &dp ,int index){
+    if(t == 0) return 0;
+    if(index == 0){
+        if(t%arr[0] == 0) return t/arr[0];
+        return 1e6;
+    }
+    if(dp[index][t] != -1) return dp[index][t];
+    int nottake = totalWays(arr , t , dp , index -1);
+
+    int take = 1e6;
+    if(arr[index]<=t) take = 1+ totalWays(arr , t-arr[index] , dp , index);
+
+    return dp[index][t] =  min(take , nottake);
+}
+    int minCoins(vector<int> &arr, int n) {
+        // code here
+        
+        
+        int size = arr.size();
+        vector<vector<int>> dp(size , vector<int>(n+1 , -1));
+        
+        
+        int r = totalWays(arr , n , dp , size-1);
+        
+        if(r == 1e6) return -1;
+        
+        return r;
+    }
+};
