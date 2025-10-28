0bc524b819baeceb3185650e6faf935da3753a00
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-10-28 22:30:18 +0530

    Time: 72 ms (6.41%), Space: 56.6 MB (46.91%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const val = init;
+    const obj = {
+        increment : ()=>{
+            return ++init;
+        },
+        decrement : ()=>{
+            return --init;
+        },
+        reset : ()=>{
+            init = val;
+            return val;
+        }
+    }
+
+    return obj;
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
