eec4c9a5c91f46bd66936f3663ae6fd2c32fb32e
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-12-07 16:15:57 +0530

    Time: 43 ms (50.34%), Space: 85 MB (10.59%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -4,23 +4,21 @@ public:
         int size = arr.size();
         int count = 0;
         int left = 0;
-        unordered_map<int , int> m;
-        for(int right = 0 ; right<size ; right++){
+        unordered_map<int, int> m;
+        for (int right = 0; right < size; right++) {
             m[arr[right]]++;
-            if(m.size() > 2){
-                while(m.size()>2){
-                    m[arr[left]]--;
-                    if(m[arr[left]] == 0) m.erase(arr[left]);
-                    left++;
-                }
+            if (m.size() > 2) {
+                m[arr[left]]--;
+                if (m[arr[left]] == 0)
+                    m.erase(arr[left]);
+                left++;
             }
 
-            if(m.size() <= 2){
-                count = max(count , right-left+1);
+            if (m.size() <= 2) {
+                count = max(count, right - left + 1);
             }
         }
 
         return count;
-        
     }
 };
