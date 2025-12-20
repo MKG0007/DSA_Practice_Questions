7e0a185202b2291e214af6636c91be338fc8f693
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-12-20 14:37:38 +0530

    Time: 58 ms (17.57%), Space: 78.5 MB (5.98%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& arr) {
-        
-        if(arr.size() == 0) return 0;
-        if(arr.size() == 1) {if(arr[0] != 1) return 1;}
+        int size = arr.size();
+        if(size == 0) return 0;
+        if(size == 1){
+            if(arr[0] != 1) return 1;
+            return arr[0]+1;
+        }
+        unordered_set<int> m;
+        int small = INT_MAX;
         int large = INT_MIN;
-        int pos = 0;
-        for(int i = 0 ; i<arr.size() ; i++){
-            if(arr[i] >= 1e6) continue;
-            large = max(large , arr[i]);
-            if(arr[i]>0) pos++;
-            
-        }
-        if(pos == 0) return 1;
-        
-        vector<int> ans(large+1 , 0);
-        for(int i = 0 ; i<arr.size() ; i++){
-            if(arr[i]>0 && arr[i]<=large){
-            ans[arr[i]]++;
+        for(int i = 0 ; i<size  ; i++){
+            if(arr[i]>= 1e6) continue;
+            if(arr[i]>0){
+                small = min(small , arr[i]);
+                large = max(small , arr[i]);
             }
+            m.insert(arr[i]);
         }
-        int num = 0;
-        for(int i = 1 ; i<ans.size() ; i++){
-            if(ans[i] == 0){
-                num = i;
-                break;
-            }
+        if(small != 1) return 1;
+        int num = small+1;
+        while(true){
+            if(m.find(num) == m.end()) break;
+            num++;
         }
-        
-        if(num == 0) return large+1;
-        
-        return num;  
+
+        return num;
+
+
     }
 };
