648005b5131a2f06f10542a6cf1fa116a2b422f5
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2026-05-27 18:04:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.cpp
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -1,19 +1,25 @@
 class Solution {
   public:
   
-  int helper(vector<int> &arr , vector<int> &dp , int index){
-      if(index == 0) return arr[0];
-      if(index<0) return 0;
-      if(dp[index] != -1 ) return dp[index];
-      int take = arr[index] + helper(arr , dp , index-2);
-      int ntake = helper(arr , dp , index-1);
+  int helper(vector<int> &arr , int index , vector<int> &dp){
+      if(index < 0) return 0;
+      if(index == 0) return arr[index];
       
-      return dp[index] = max(take , ntake);
+      if(dp[index] != -1) return dp[index];
+      int pick = arr[index] + helper(arr , index-2 , dp);
+      
+      int npick = helper(arr , index-1 , dp);
+      
+      
+      return dp[index] = max(pick , npick);
+      
+      
   }
     int findMaxSum(vector<int>& arr) {
         // code here
         int size = arr.size();
         vector<int> dp(size , -1);
-        return helper(arr , dp , size-1);
+        return helper(arr , size-1 , dp);
+        
     }
 };
