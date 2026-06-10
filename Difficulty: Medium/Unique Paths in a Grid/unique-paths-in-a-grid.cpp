7a977c1108ca529241da746aa230bfcc082ea2a8
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2026-06-10 10:49:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.cpp
+++ Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.cpp
@@ -1,23 +1,23 @@
 class Solution {
   public:
   
-  int helper(vector<vector<int>> &arr , int i , int j , int row , int col , vector<vector<int>> &dp){
-      if(i<0 || j<0 || i>=row || j>=col || arr[i][j] == 1) return 0;
+  int helper(vector<vector<int>> &arr , int row , int col , int i , int j , vector<vector<int>> &dp){
+      if(i == 0 && j == 0 && arr[i][j] != 1) return 1;
       
-      if(i == 0 && j == 0) return 1;
+      if(i<0 || j<0 || arr[i][j] == 1) return 0;
       
       if(dp[i][j] != -1) return dp[i][j];
-      int up = helper(arr , i-1 , j , row , col , dp);
+      int left = arr[i][j] + helper(arr , row , col , i , j-1 , dp);
       
-      int left = helper(arr , i , j-1 , row , col , dp);
+      int up = arr[i][j] + helper(arr , row , col , i-1 , j , dp);
       
-      return dp[i][j] = up +left;
+      return dp[i][j] = left + up;
   }
     int uniquePaths(vector<vector<int>> &grid) {
-        // code here
         int row = grid.size();
         int col = grid[0].size();
         vector<vector<int>> dp(row , vector<int>(col , -1));
-        return helper(grid , row-1 , col-1 , row , col , dp);
+        return helper(grid , row , col , row-1 , col-1 , dp);
+        
     }
 };
